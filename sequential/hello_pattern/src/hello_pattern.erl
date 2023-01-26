ee32c26e220ef6f0fe9295b1738f56fcafdfe2b6
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-26 17:20:01 -0300

    Add solution for hello_pattern

--- sequential/hello_pattern/src/hello_pattern.erl
+++ sequential/hello_pattern/src/hello_pattern.erl
@@ -2,5 +2,15 @@
 
 -export([hello/1]).
 
-hello({tuple_element_1, tuple_element_2}) ->
-  resolve.
+hello({morning, _}) ->
+  morning;
+hello({evening, Name}) ->
+  {good, evening, Name};
+hello({night, _}) ->
+  night;
+hello({math_class, Number, _}) when Number < 0 ->
+  none;
+hello({math_class, _, Name}) ->
+  {math_class, Name};
+hello(_) ->
+  none.
