dc78ea8a3342a9698bdddee8481b844890df8e34
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-27 16:13:31 -0300

    Add filter_fibonacci_numbers

--- sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
+++ sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
@@ -2,5 +2,11 @@
 
 -export([filter/1]).
 
+is_perfect_square(X) ->
+  trunc(math:sqrt(X)) == math:sqrt(X).
+
+is_fibonacci_number(X) ->
+  is_perfect_square(5 * math:pow(X,2) + 4) or is_perfect_square(5 * math:pow(X,2) - 4).
+
 filter(List) ->
-  put_your_solution_here.
+  [X || X <- List, is_fibonacci_number(X)].
