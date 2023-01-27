a8a6f4898acd9aec4bee6904a491805310f4f001
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-27 15:28:49 -0300

    Add calculate_bmi

--- sequential/calculate_bmi/src/calculate_bmi.erl
+++ sequential/calculate_bmi/src/calculate_bmi.erl
@@ -5,7 +5,16 @@
 -include("../src/person_record.hrl").
 
 bmi(Person) ->
-  put_your_solution_here.
+  Person#person.weight / (math:pow(Person#person.height, 2)).
+
+get_classification(BMI) when BMI < 18.4 ->
+  underweight;
+get_classification(BMI) when BMI < 24.9 ->
+  normal;
+get_classification(BMI) when BMI < 29.9 ->
+  overweight;
+get_classification(_) ->
+  obese.
 
 classify(Person) ->
-  put_your_solution_here.
+  get_classification(bmi(Person)).
