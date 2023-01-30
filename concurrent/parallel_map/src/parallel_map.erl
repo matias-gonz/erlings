12441d24b0c5d0d2d9963de00e9b454cc265a568
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-30 11:02:13 -0300

    Add concurrent:parallel_map

--- concurrent/parallel_map/src/parallel_map.erl
+++ concurrent/parallel_map/src/parallel_map.erl
@@ -1,5 +1,17 @@
 -module(parallel_map).
--export([pmap/2]).
+-export([pmap/2, process_element/3]).
+
+process_element(From, Fun, Element) ->
+  From ! {self(), Fun(Element)}.
 
 pmap(Fun, List) ->
-  your_solution_here.
+  pmap(Fun, List, []).
+
+pmap(_, [], Acc) ->
+  Acc;
+pmap(Fun, [H|T], Acc) ->
+  Pid = spawn(parallel_map, process_element, [self(), Fun, H]),
+  receive
+    {Pid, Element} ->
+      pmap(Fun, T, Acc ++ [Element])
+    end.
