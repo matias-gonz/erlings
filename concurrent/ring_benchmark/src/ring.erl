be39285015d025327450cd0a07e99f7c83a7259b
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-30 12:33:56 -0300

    Add concurrent:ring_benchmark

--- concurrent/ring_benchmark/src/ring.erl
+++ concurrent/ring_benchmark/src/ring.erl
@@ -2,5 +2,21 @@
 -export([ring/2]).
 
 %% N processes, M messages
-ring(_N, _M) ->
-  complete_here.
+ring(N, M) ->
+  Parent = self(),
+  Processes = [spawn_link(fun () -> node_ring(Parent) end)
+               || _ <- lists:seq(1, N)],
+  [H|T] = lists:append(lists:duplicate(M, Processes)),
+  H ! {msg, T},
+  receive
+    done ->
+      done
+  end.
+  
+node_ring(Parent) ->
+  receive
+    {msg, []} -> Parent ! done;
+    {msg, [H|T]} ->
+      H ! {msg, T},
+      node_ring(Parent)
+    end.
