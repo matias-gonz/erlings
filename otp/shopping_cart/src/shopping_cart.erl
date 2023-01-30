debd78cd1cd453d4eb5721fd1f8dee33f790d963
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-30 13:17:35 -0300

    Add otp:shopping_cart

--- otp/shopping_cart/src/shopping_cart.erl
+++ otp/shopping_cart/src/shopping_cart.erl
@@ -6,16 +6,31 @@
          put_item/2,
          finish/1,
          cost_so_far/1,
+         init/1,
+         handle_call/3,
+         handle_cast/2
         ]).
 
 start_link() ->
-     put_your_solution_here.
+     gen_server:start_link(?MODULE, [], []).
 
 put_item(Pid, Item) ->
-    put_your_solution_here.
+    gen_server:call(Pid, {put, Item}).
 
 cost_so_far(Pid) ->
-    put_your_solution_here.
+    gen_server:call(Pid, cost).
 
 finish(Pid) ->
-    put_your_solution_here.
+    gen_server:call(Pid, terminate).
+
+init([]) -> {ok, []}.
+
+handle_call(terminate, _From, Items) ->
+    {stop, normal, ok, Items};
+handle_call(cost, _From, Items) ->
+    {reply, lists:foldl(fun({_Name, Price}, Sum) -> Price + Sum end, 0, Items), Items};
+handle_call({put, Item}, _From, Items) ->
+    {reply, [Item|Items], [Item|Items]}.
+
+handle_cast(_, _) ->
+    {noreply, ok}.
